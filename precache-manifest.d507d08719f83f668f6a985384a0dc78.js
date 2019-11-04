374a6554b535ae76d5eff394775ad5fd227b5e6d
AD <ad.suleman@gmail.com>
2019-11-04 12:35:42 +0100

    Updates

---
+++ precache-manifest.d507d08719f83f668f6a985384a0dc78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff789bc91210ed9c012cf6b9168bbad",
+    "url": "/adsulemann/React-deploy-classic.git/index.html"
+  },
+  {
+    "revision": "67ff24621a5bd4afa89a",
+    "url": "/adsulemann/React-deploy-classic.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "53aa32124ba884d18ec8",
+    "url": "/adsulemann/React-deploy-classic.git/static/js/2.34ab973b.chunk.js"
+  },
+  {
+    "revision": "67ff24621a5bd4afa89a",
+    "url": "/adsulemann/React-deploy-classic.git/static/js/main.5274bcb4.chunk.js"
+  },
+  {
+    "revision": "ac921c99658ec0150763",
+    "url": "/adsulemann/React-deploy-classic.git/static/js/runtime-main.bd6d5086.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/adsulemann/React-deploy-classic.git/static/media/logo.25bf045c.svg"
+  }
+]);
